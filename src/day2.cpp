77bc20605a28227f81018012971f3683b935ec8e
David Pfahler <pfahler.david@gmail.com>
2020-12-01 23:12:39 +0100

    prepared day 2

---
+++ src/day2.cpp
@@ -0,0 +1,23 @@
+#include "days.hpp"
+
+#include <range/v3/all.hpp>
+
+void solveDay(
+  [[maybe_unused]] std::istream &input,
+  [[maybe_unused]] std::ostream &output)
+{
+}
+
+template<>
+void days::solve<days::Day::Day2, days::Part::Part1>(std::istream &input,
+  std::ostream &output)
+{
+  solveDay(input, output);
+}
+
+template<>
+void days::solve<days::Day::Day2, days::Part::Part2>(std::istream &input,
+  std::ostream &output)
+{
+  solveDay(input, output);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "days.hpp"
 
-static constexpr days::Day CurrentDay = days::Day::Day1;
+static constexpr days::Day CurrentDay = days::Day::Day2;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
 {
-  spdlog::info("Hello, {}!", "World");
+  spdlog::info("Advent of Code 2020");
   days::solveProblem<CurrentDay, days::Part::Part1>();
   days::solveProblem<CurrentDay, days::Part::Part2>();
 }
