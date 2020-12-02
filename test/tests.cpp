c7ec3224b3429423639d5ee8c39fc9873b355186
David Pfahler <pfahler.david@gmail.com>
2020-12-02 18:29:16 +0100

    now supports running unit tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,33 @@
+#include <sstream>
 #include <catch2/catch.hpp>
 
-unsigned int Factorial(unsigned int number)
+#include "days.hpp"
+
+
+TEST_CASE("Day 1 Part 1", "[days]")
 {
-  return number <= 1 ? number : Factorial(number - 1) * number;
+  std::stringstream ss{"1721\n979\n366\n299\n675\n1456"};
+  auto const out = days::solve<days::day::day_1, days::part::part_1>(ss);
+  REQUIRE(out == "514579");
 }
 
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST_CASE("Day 1 Part 2", "[days]")
 {
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+  std::stringstream ss{ "1721\n979\n366\n299\n675\n1456" };
+  auto const out = days::solve<days::day::day_1, days::part::part_2>(ss);
+  REQUIRE(out == "241861950");
+}
+
+TEST_CASE("Day 2 Part 1", "[days]")
+{
+  std::stringstream ss{ "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc" };
+  auto const out = days::solve<days::day::day_2, days::part::part_1>(ss);
+  REQUIRE(out == "2");
+}
+
+TEST_CASE("Day 2 Part 2", "[days]")
+{
+  std::stringstream ss{ "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc" };
+  auto const out = days::solve<days::day::day_2, days::part::part_2>(ss);
+  REQUIRE(out == "1");
 }
