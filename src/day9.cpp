ae0d10d229d26f593c5cf8e8a1528f7d27f06061
David Pfahler <pfahler.david@gmail.com>
2020-12-09 09:05:33 +0100

    solved day 9

--- src/day9.cpp
+++ src/day9.cpp
@@ -1,28 +1,81 @@
+#include <deque>
+
 #include "days.hpp"
 
 #include <range/v3/all.hpp>
 
-using groups = std::vector<std::string>;
+#include "input_utils.hpp"
 
-auto parse_input(std::istream &input) -> groups
+static constexpr size_t preamble_size = 25;
+using preamble = std::array<int, preamble_size>;
+
+auto find_pair(preamble &p, size_t &current_pos, const int num) -> bool
 {
-  groups groups =
-    ranges::istream_view<std::string>(input) | ranges::to<std::vector>();
-  return groups;
+  auto sorted{ p };
+  ranges::sort(sorted);
+  auto l = std::begin(sorted);
+  auto r = std::prev(std::end(sorted));
+  auto found = false;
+  while (l < r) {
+    auto const sum = *l + *r;
+    if (sum == num) {
+      found = true;
+      break;
+    }
+    if (sum < num)
+      ++l;
+    else
+      --r;
+  }
+  if (!found) return true;
+  p[current_pos] = num;
+  current_pos = (current_pos + 1) % preamble_size;
+  return false;
+}
+
+auto find_wrong_number(std::vector<int> const &numbers) -> auto
+{
+  preamble preamble{};
+  std::copy_n(numbers.begin(), preamble_size, preamble.begin());
+  size_t current_pos = 0;
+  auto const found = std::find_if(numbers.begin() + preamble_size,
+    numbers.end(),
+    [&preamble, &current_pos](
+      const int value) { return find_pair(preamble, current_pos, value); });
+  return found;
 }
 
 template<>
 auto days::solve<days::day::day_9, days::part::part_1>(std::istream &input)
   -> std::string
 {
-  auto const groups = parse_input(input);
-  return std::to_string(groups.size());
+  auto const numbers = utils::split<int>(input);
+  auto const found = find_wrong_number(numbers);
+  return std::to_string(*found);
 }
 
 template<>
 auto days::solve<days::day::day_9, days::part::part_2>(std::istream &input)
   -> std::string
 {
-  auto const groups = parse_input(input);
-  return std::to_string(groups.size());
+  auto const numbers = utils::split<int>(input);
+  auto const found = *find_wrong_number(numbers);
+
+  std::deque<int> range{};
+  auto sum = 0;
+  auto range_end = std::find_if(begin(numbers),
+    end(numbers),
+    [found, &sum, &range](auto const num) {
+      sum += num;
+      range.push_back(num);
+      if (found == sum) { return true; }
+      while (sum > found) {
+        auto const f = range.front();
+        sum -= f;
+        range.pop_front();
+      }
+      return found == sum;
+    });
+  const auto [min, max] = ranges::minmax_element(range);
+  return std::to_string(*min + *max);
 }
--- test/tests.cpp
+++ test/tests.cpp
@@ -192,17 +192,20 @@ TEST_CASE("Day 8 Part 2", "[days]")
 TEST_CASE("Day 9 Part 1", "[days]")
 {
   std::stringstream ss{
-    ""
+    "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299"
+    "\n277\n309\n576"
   };
   auto const out = days::solve<days::day::day_9, days::part::part_1>(ss);
-  REQUIRE(out == "5");
+  REQUIRE(out == "127");
 }
 
 TEST_CASE("Day 9 Part 2", "[days]")
 {
   std::stringstream ss{
-    ""
+    "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299"
+    "\n277\n309\n576"
+
   };
   auto const out = days::solve<days::day::day_9, days::part::part_2>(ss);
-  REQUIRE(out == "8");
+  REQUIRE(out == "62");
 }
