9bf4a1dd8093d2da18fca9da0d8c16b3b9fec8d0
David Pfahler <pfahler.david@gmail.com>
2020-12-10 10:19:32 +0100

    more refactoring on day 9

--- src/day9.cpp
+++ src/day9.cpp
@@ -1,79 +1,19 @@
-#include <deque>
-
 #include "days.hpp"
+#include "day9.hpp"
 
 #include <range/v3/all.hpp>
-
-#include "input_utils.hpp"
-
-static constexpr size_t preamble_size = 25;
-using preamble = std::array<int, preamble_size>;
-
-auto find_pair(preamble &p, size_t &current_pos, const int num) -> bool
-{
-  auto sorted{ p };
-  ranges::sort(sorted);
-  auto l = std::begin(sorted);
-  auto r = std::prev(std::end(sorted));
-  auto found = false;
-  while (l < r) {
-    auto const sum = *l + *r;
-    if (sum == num) {
-      found = true;
-      break;
-    }
-    if (sum < num)
-      ++l;
-    else
-      --r;
-  }
-  if (!found) return true;
-  p[current_pos] = num;
-  current_pos = (current_pos + 1) % preamble_size;
-  return false;
-}
-
-auto find_wrong_number(std::vector<int> const &numbers) -> auto
-{
-  preamble preamble{};
-  std::copy_n(numbers.begin(), preamble_size, preamble.begin());
-  size_t current_pos = 0;
-  auto const found =
-    ranges::find_if(numbers | ranges::views::drop(preamble_size),
-    [&preamble, &current_pos](
-      const int value) { return find_pair(preamble, current_pos, value); });
-  return found;
-}
+#include <deque>
 
 template<>
 auto days::solve<days::day::day_9, days::part::part_1>(std::istream &input)
   -> std::string
 {
-  auto const numbers = utils::split<int>(input);
-  auto const found = find_wrong_number(numbers);
-  return std::to_string(*found);
+  return preamble_finder<25>::solve_part1(input);
 }
 
 template<>
 auto days::solve<days::day::day_9, days::part::part_2>(std::istream &input)
   -> std::string
 {
-  auto const numbers = utils::split<int>(input);
-  auto const found = *find_wrong_number(numbers);
-
-  std::deque<int> range{};
-  auto sum = 0;
-  auto range_end =
-    ranges::find_if(numbers, [found, &sum, &range](auto const num) {
-      sum += num;
-      range.push_back(num);
-      while (sum > found) {
-        auto const f = range.front();
-        sum -= f;
-        range.pop_front();
-      }
-      return found == sum;
-    });
-  const auto [min, max] = ranges::minmax_element(range);
-  return std::to_string(*min + *max);
+  return preamble_finder<25>::solve_part2(input);
 }
